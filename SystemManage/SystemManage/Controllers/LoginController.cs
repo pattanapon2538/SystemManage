063ca0c0dda8e1fdb0ba2f458fdeb389f899d81c
CPE59346 <pattanapon2538@outlook.com>
2018-09-24 16:20:06 +0700

    Session Admin Login

--- SystemManage/SystemManage/Controllers/LoginController.cs
+++ SystemManage/SystemManage/Controllers/LoginController.cs
@@ -15,16 +15,15 @@ namespace SystemManage.Controllers
         {
             return View();
         }
-        public ActionResult CheckLogin(string User_Email, string User_Password,string LoginErrorMessage)
+        public ActionResult CheckLogin(string User_Email, string User_Password)
         {
             if(!string.IsNullOrEmpty(User_Email) && !string.IsNullOrEmpty(User_Password))
             {
                 var user = db.Users.Where(c => c.User_Email == User_Email && c.User_Password == User_Password).FirstOrDefault();
                 if (user != null)
                 {
-                    Session["UserName"] = user.User_Email;       
-                    Session["Fullname"] = user.User_Name + " " + user.User_LastName;       
-                    return RedirectToAction("Index","Member");
+                    Session["userName"] = user.User_Email;
+                    return RedirectToAction("Index", "Member"); 
                 }
             }
             return View("Index");
